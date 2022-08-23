2aa0bc37188322294f91ebf9befedbf89300b9fa
Pedro Holanda <pedroholanda@gmail.com>
2022-08-23 11:49:03 +0200

    More R dependencies

--- test/r/dependencies.R
+++ test/r/dependencies.R
@@ -1,1 +1,3 @@
 install.packages("testthat", repos = "https://cloud.r-project.org", pkgType = "binary")
+install.packages("DBI", repos = "https://cloud.r-project.org", pkgType = "binary")
+install.packages("dbplyr", repos = "https://cloud.r-project.org", pkgType = "binary")
