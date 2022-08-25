d0e705add394285908d0d02267c1d468191e5849
Pedro Holanda <pedroholanda@gmail.com>
2022-08-25 15:32:36 +0200

    It seems that BUILD_R is borked for extension loading?

--- test/r/dependencies.R
+++ test/r/dependencies.R
@@ -1,3 +1,4 @@
 install.packages("testthat", repos = "https://cloud.r-project.org", pkgType = "binary")
 install.packages("DBI", repos = "https://cloud.r-project.org", pkgType = "binary")
 install.packages("dbplyr", repos = "https://cloud.r-project.org", pkgType = "binary")
+install.packages("arrow", repos = "https://cloud.r-project.org", pkgType = "binary")
