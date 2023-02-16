507f629f84f90db4d0dfc529dae444c84c9f3c6d
Lindsay Wray <lindsay@lindsays-mbp.home>
2023-02-16 14:05:22 +0100

    temporarily skip related python tests

--- test/python/test_enable_optimizer.py
+++ test/python/test_enable_optimizer.py
@@ -1,87 +1,93 @@
 import pandas as pd
 import duckdb
 
-# def test_optimizer_defaults_to_true(require):
-#     connection = require('substrait')
+@pytest.mark.skip(reason="needs to be merged in DuckDB repo first")
+def test_optimizer_defaults_to_true(require):
+    connection = require('substrait')
 
-#     connection.execute('CREATE TABLE integers (i INTEGER)')
-#     connection.execute('INSERT INTO integers VALUES (0),(1),(2),(3),(4),(5),(6),(7),(8),(9),(NULL)')
+    connection.execute('CREATE TABLE integers (i INTEGER)')
+    connection.execute('INSERT INTO integers VALUES (0),(1),(2),(3),(4),(5),(6),(7),(8),(9),(NULL)')
 
-#     res1 = connection.get_substrait("SELECT abs(i) FROM integers LIMIT 5")
-#     proto_bytes1 = res1.fetchone()[0]
+    res1 = connection.get_substrait("SELECT abs(i) FROM integers LIMIT 5")
+    proto_bytes1 = res1.fetchone()[0]
 
-#     res2 = connection.get_substrait("SELECT abs(i) FROM integers LIMIT 5", enable_optimizer=True)
-#     proto_bytes2 = res2.fetchone()[0]
+    res2 = connection.get_substrait("SELECT abs(i) FROM integers LIMIT 5", enable_optimizer=True)
+    proto_bytes2 = res2.fetchone()[0]
 
-#     assert proto_bytes1 == proto_bytes2
+    assert proto_bytes1 == proto_bytes2
 
-# def test_optimizer_with_abs(require):
-#     connection = require('substrait')
+@pytest.mark.skip(reason="needs to be merged in DuckDB repo first")
+def test_optimizer_with_abs(require):
+    connection = require('substrait')
 
-#     connection.execute('CREATE TABLE integers (i INTEGER)')
-#     connection.execute('INSERT INTO integers VALUES (0),(1),(2),(3),(4),(5),(6),(7),(8),(9),(NULL)')
+    connection.execute('CREATE TABLE integers (i INTEGER)')
+    connection.execute('INSERT INTO integers VALUES (0),(1),(2),(3),(4),(5),(6),(7),(8),(9),(NULL)')
 
-#     res1 = connection.get_substrait("SELECT abs(i) FROM integers LIMIT 5", enable_optimizer=True)
-#     proto_bytes1 = res1.fetchone()[0]
+    res1 = connection.get_substrait("SELECT abs(i) FROM integers LIMIT 5", enable_optimizer=True)
+    proto_bytes1 = res1.fetchone()[0]
 
-#     res2 = connection.get_substrait("SELECT abs(i) FROM integers LIMIT 5", enable_optimizer=False)
-#     proto_bytes2 = res2.fetchone()[0]
+    res2 = connection.get_substrait("SELECT abs(i) FROM integers LIMIT 5", enable_optimizer=False)
+    proto_bytes2 = res2.fetchone()[0]
 
-#     assert proto_bytes1 != proto_bytes2
+    assert proto_bytes1 != proto_bytes2
 
-# def test_optimizer_with_like(require):
-#     connection = require('substrait')
+@pytest.mark.skip(reason="needs to be merged in DuckDB repo first")
+def test_optimizer_with_like(require):
+    connection = require('substrait')
 
-#     connection.execute('CREATE TABLE varchars (v VARCHAR)')
-#     connection.execute('INSERT INTO varchars VALUES (\'ducky\'), (\'mcDuck\'), (\'Duckster\')')
+    connection.execute('CREATE TABLE varchars (v VARCHAR)')
+    connection.execute('INSERT INTO varchars VALUES (\'ducky\'), (\'mcDuck\'), (\'Duckster\')')
 
-#     res1 = connection.get_substrait("SELECT * FROM varchars WHERE v LIKE '%y'", enable_optimizer=True)
-#     proto_bytes1 = res1.fetchone()[0]
+    res1 = connection.get_substrait("SELECT * FROM varchars WHERE v LIKE '%y'", enable_optimizer=True)
+    proto_bytes1 = res1.fetchone()[0]
 
-#     res2 = connection.get_substrait("SELECT * FROM varchars WHERE v LIKE '%y'", enable_optimizer=False)
-#     proto_bytes2 = res2.fetchone()[0]
+    res2 = connection.get_substrait("SELECT * FROM varchars WHERE v LIKE '%y'", enable_optimizer=False)
+    proto_bytes2 = res2.fetchone()[0]
 
-#     assert proto_bytes1 != proto_bytes2
+    assert proto_bytes1 != proto_bytes2
 
-# # Now test get_substrait_json
-# def test_optimizer_json_defaults_to_true(require):
-#     connection = require('substrait')
+# Now test get_substrait_json
+@pytest.mark.skip(reason="needs to be merged in DuckDB repo first")
+def test_optimizer_json_defaults_to_true(require):
+    connection = require('substrait')
 
-#     connection.execute('CREATE TABLE integers (i INTEGER)')
-#     connection.execute('INSERT INTO integers VALUES (0),(1),(2),(3),(4),(5),(6),(7),(8),(9),(NULL)')
+    connection.execute('CREATE TABLE integers (i INTEGER)')
+    connection.execute('INSERT INTO integers VALUES (0),(1),(2),(3),(4),(5),(6),(7),(8),(9),(NULL)')
 
-#     res1 = connection.get_substrait_json("SELECT abs(i) FROM integers LIMIT 5")
-#     proto_bytes1 = res1.fetchone()[0]
+    res1 = connection.get_substrait_json("SELECT abs(i) FROM integers LIMIT 5")
+    proto_bytes1 = res1.fetchone()[0]
 
-#     res2 = connection.get_substrait_json("SELECT abs(i) FROM integers LIMIT 5", enable_optimizer=True)
-#     proto_bytes2 = res2.fetchone()[0]
+    res2 = connection.get_substrait_json("SELECT abs(i) FROM integers LIMIT 5", enable_optimizer=True)
+    proto_bytes2 = res2.fetchone()[0]
 
-#     assert proto_bytes1 == proto_bytes2
+    assert proto_bytes1 == proto_bytes2
 
-# def test_optimizer_json_with_abs(require):
-#     connection = require('substrait')
+@pytest.mark.skip(reason="needs to be merged in DuckDB repo first")
+def test_optimizer_json_with_abs(require):
+    connection = require('substrait')
 
-#     connection.execute('CREATE TABLE integers (i INTEGER)')
-#     connection.execute('INSERT INTO integers VALUES (0),(1),(2),(3),(4),(5),(6),(7),(8),(9),(NULL)')
+    connection.execute('CREATE TABLE integers (i INTEGER)')
+    connection.execute('INSERT INTO integers VALUES (0),(1),(2),(3),(4),(5),(6),(7),(8),(9),(NULL)')
 
-#     res1 = connection.get_substrait_json("SELECT abs(i) FROM integers LIMIT 5", enable_optimizer=True)
-#     proto_bytes1 = res1.fetchone()[0]
+    res1 = connection.get_substrait_json("SELECT abs(i) FROM integers LIMIT 5", enable_optimizer=True)
+    proto_bytes1 = res1.fetchone()[0]
 
-#     res2 = connection.get_substrait_json("SELECT abs(i) FROM integers LIMIT 5", enable_optimizer=False)
-#     proto_bytes2 = res2.fetchone()[0]
+    res2 = connection.get_substrait_json("SELECT abs(i) FROM integers LIMIT 5", enable_optimizer=False)
+    proto_bytes2 = res2.fetchone()[0]
 
-#     assert proto_bytes1 != proto_bytes2
+    assert proto_bytes1 != proto_bytes2
 
-# def test_optimizer_json_with_like(require):
-#     connection = require('substrait')
+@pytest.mark.skip(reason="needs to be merged in DuckDB repo first")
+def test_optimizer_json_with_like(require):
+    connection = require('substrait')
 
-#     connection.execute('CREATE TABLE varchars (v VARCHAR)')
-#     connection.execute('INSERT INTO varchars VALUES (\'ducky\'), (\'mcDuck\'), (\'Duckster\')')
+    connection.execute('CREATE TABLE varchars (v VARCHAR)')
+    connection.execute('INSERT INTO varchars VALUES (\'ducky\'), (\'mcDuck\'), (\'Duckster\')')
 
-#     res1 = connection.get_substrait_json("SELECT * FROM varchars WHERE v LIKE '%y'", enable_optimizer=True)
-#     proto_bytes1 = res1.fetchone()[0]
+    res1 = connection.get_substrait_json("SELECT * FROM varchars WHERE v LIKE '%y'", enable_optimizer=True)
+    proto_bytes1 = res1.fetchone()[0]
 
-#     res2 = connection.get_substrait_json("SELECT * FROM varchars WHERE v LIKE '%y'", enable_optimizer=False)
-#     proto_bytes2 = res2.fetchone()[0]
+    res2 = connection.get_substrait_json("SELECT * FROM varchars WHERE v LIKE '%y'", enable_optimizer=False)
+    proto_bytes2 = res2.fetchone()[0]
 
-#     assert proto_bytes1 != proto_bytes2
+    assert proto_bytes1 != proto_bytes2
