b4edc0664f2c66474f7a95e27ae395cc952e1f01
Tishj <t_b@live.nl>
2023-06-22 15:17:45 +0200

    fix python CI (hopefully last commit)

--- test/python/conftest.py
+++ test/python/conftest.py
@@ -19,9 +19,8 @@ def duckdb_empty_cursor(request):
 @pytest.fixture(scope="function")
 def require():
     def _require(extension_name, db_name=''):
-        # This is completely unnecessary now that we statically link substrait into the build
-        # but this function is kept so we don't have to change every existing test
-        conn = duckdb.connect(db_name)
+        conn = duckdb.connect(db_name, config={'allow_unsigned_extensions' : 'true'})
+        conn.execute(f"LOAD '{dir}/../../duckdb/build/{build_type}/extension/substrait/substrait.duckdb_extension'")
         return conn
 
     return _require
