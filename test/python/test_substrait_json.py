e59bf991e900b9bf9c212b2766f1ab81862419d0
Pedro Holanda <pedroholanda@gmail.com>
2024-07-11 16:43:32 +0200

    Hopefully fixing Python R CI

--- test/python/test_substrait_json.py
+++ test/python/test_substrait_json.py
@@ -8,6 +8,6 @@ def test_substrait_json(require):
 
     connection.execute('CREATE TABLE integers (i integer)')
     json =  connection.get_substrait_json("select * from integers limit 5").fetchone()[0]
-    expected_result = '{"relations":[{"root":{"input":{"fetch":{"input":{"project":{"input":{"read":{"baseSchema":{"names":["i"],"struct":{"types":[{"i32":{"nullability":"NULLABILITY_NULLABLE"}}],"nullability":"NULLABILITY_REQUIRED"}},"projection":{"select":{"structItems":[{}]},"maintainSingularStruct":true},"namedTable":{"names":["integers"]}}},"expressions":[{"selection":{"directReference":{"structField":{}},"rootReference":{}}}]}},"count":"5"}},"names":["i"]}}],"version":{"minorNumber":48,"producer":"DuckDB"}}'
+    expected_result = '{"relations":[{"root":{"input":{"project":{"input":{"fetch":{"input":{"read":{"baseSchema":{"names":["i"],"struct":{"types":[{"i32":{"nullability":"NULLABILITY_NULLABLE"}}],"nullability":"NULLABILITY_REQUIRED"}},"projection":{"select":{"structItems":[{}]},"maintainSingularStruct":true},"namedTable":{"names":["integers"]}}},"count":"5"}},"expressions":[{"selection":{"directReference":{"structField":{}},"rootReference":{}}}]}},"names":["i"]}}],"version":{"minorNumber":48,"producer":"DuckDB"}}'
     assert json == expected_result
 
