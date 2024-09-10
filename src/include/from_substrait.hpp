62316a6bad9a1934f96f5f354baa7de83886388f
Pedro Holanda <pedroholanda@gmail.com>
2024-09-10 16:05:23 +0200

    Make projection finding function optional

--- src/include/from_substrait.hpp
+++ src/include/from_substrait.hpp
@@ -58,6 +58,6 @@ private:
 	//! names
 	static const unordered_map<std::string, std::string> function_names_remap;
 	static const case_insensitive_set_t valid_extract_subfields;
-	vector<ParsedExpression*> struct_expressions;
+	vector<ParsedExpression *> struct_expressions;
 };
 } // namespace duckdb
