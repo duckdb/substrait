d488993cf213cc66cdc88d34c7c9e1123b95dbfd
Pedro Holanda <pedroholanda@gmail.com>
2024-09-24 19:03:18 +0200

    Add support for structs from nested functions if we can refeer to original naming from root

--- src/include/from_substrait.hpp
+++ src/include/from_substrait.hpp
@@ -16,6 +16,42 @@
 
 namespace duckdb {
 
+struct RootNameIterator {
+	explicit RootNameIterator(const google::protobuf::RepeatedPtrField<std::string> *names) : names(names) {};
+	string GetCurrentName() const {
+		if (!names) {
+			return "";
+		}
+		if (iterator >= names->size()) {
+			throw InvalidInputException("Trying to access invalid root name at struct creation");
+		}
+		return (*names)[iterator];
+	}
+	void Next() {
+		++iterator;
+	}
+	bool Unique(idx_t count) const {
+		idx_t pos = iterator;
+		set<string> values;
+		for (idx_t i = 0; i < count; i++) {
+			if (values.find((*names)[pos]) != values.end()) {
+				return false;
+			}
+			values.insert((*names)[pos]);
+			pos++;
+		}
+		return true;
+	}
+	bool Finished() const {
+		if (!names) {
+			return true;
+		}
+		return iterator >= names->size();
+	}
+	const google::protobuf::RepeatedPtrField<std::string> *names = nullptr;
+	int iterator = 0;
+};
+
 class SubstraitToDuckDB {
 public:
 	SubstraitToDuckDB(shared_ptr<ClientContext> &context_p, const string &serialized, bool json = false,
@@ -27,26 +63,33 @@ private:
 	//! Transforms Substrait Plan Root To a DuckDB Relation
 	shared_ptr<Relation> TransformRootOp(const substrait::RelRoot &sop);
 	//! Transform Substrait Operations to DuckDB Relations
-	shared_ptr<Relation> TransformOp(const substrait::Rel &sop);
+	shared_ptr<Relation> TransformOp(const substrait::Rel &sop,
+	                                 const google::protobuf::RepeatedPtrField<std::string> *names = nullptr);
 	shared_ptr<Relation> TransformJoinOp(const substrait::Rel &sop);
 	shared_ptr<Relation> TransformCrossProductOp(const substrait::Rel &sop);
-	shared_ptr<Relation> TransformFetchOp(const substrait::Rel &sop);
+	shared_ptr<Relation> TransformFetchOp(const substrait::Rel &sop,
+	                                      const google::protobuf::RepeatedPtrField<std::string> *names = nullptr);
 	shared_ptr<Relation> TransformFilterOp(const substrait::Rel &sop);
-	shared_ptr<Relation> TransformProjectOp(const substrait::Rel &sop);
+	shared_ptr<Relation> TransformProjectOp(const substrait::Rel &sop,
+	                                        const google::protobuf::RepeatedPtrField<std::string> *names = nullptr);
 	shared_ptr<Relation> TransformAggregateOp(const substrait::Rel &sop);
 	shared_ptr<Relation> TransformReadOp(const substrait::Rel &sop);
-	shared_ptr<Relation> TransformSortOp(const substrait::Rel &sop);
-	shared_ptr<Relation> TransformSetOp(const substrait::Rel &sop);
+	shared_ptr<Relation> TransformSortOp(const substrait::Rel &sop,
+	                                     const google::protobuf::RepeatedPtrField<std::string> *names = nullptr);
+	shared_ptr<Relation> TransformSetOp(const substrait::Rel &sop,
+	                                    const google::protobuf::RepeatedPtrField<std::string> *names = nullptr);
 
 	//! Transform Substrait Expressions to DuckDB Expressions
-	unique_ptr<ParsedExpression> TransformExpr(const substrait::Expression &sexpr);
+	unique_ptr<ParsedExpression> TransformExpr(const substrait::Expression &sexpr,
+	                                           RootNameIterator *iterator = nullptr);
 	static unique_ptr<ParsedExpression> TransformLiteralExpr(const substrait::Expression &sexpr);
 	static unique_ptr<ParsedExpression> TransformSelectionExpr(const substrait::Expression &sexpr);
 	unique_ptr<ParsedExpression> TransformScalarFunctionExpr(const substrait::Expression &sexpr);
 	unique_ptr<ParsedExpression> TransformIfThenExpr(const substrait::Expression &sexpr);
 	unique_ptr<ParsedExpression> TransformCastExpr(const substrait::Expression &sexpr);
 	unique_ptr<ParsedExpression> TransformInExpr(const substrait::Expression &sexpr);
-	unique_ptr<ParsedExpression> TransformNested(const substrait::Expression &sexpr);
+	unique_ptr<ParsedExpression> TransformNested(const substrait::Expression &sexpr,
+	                                             RootNameIterator *iterator = nullptr);
 
 	static void VerifyCorrectExtractSubfield(const string &subfield);
 	static string RemapFunctionName(const string &function_name);
